81c547241d17e11f16f515e51bd285ad8098ef6a
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-02-23 17:07:17 +0530

    Modified LeaveType Entity to have maximum length of 25 characters for Name Property

--- LeaveManagementSystem.Web/Data/LeaveType.cs
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -1,8 +1,13 @@
-﻿namespace LeaveManagementSystem.Web.Data
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
 {
     public class LeaveType
     {
         public int Id { get; set; }
+        //[Column(TypeName ="nvarchar(150)")]
+        [MaxLength(25)]
         public string Name { get; set; }
         public int NumberOfDays { get; set; }
     }
