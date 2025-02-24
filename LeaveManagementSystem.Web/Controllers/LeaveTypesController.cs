2f8cb85c24fe695ce54ef34360e5f528920d672e
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-02-24 20:08:52 +0530

    Modified LeaveTypes Controller Index Action Method to make use of AutoMapper to pass onto View

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -7,16 +7,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LeaveManagementSystem.Web.Data;
 using LeaveManagementSystem.Web.Models.LeaveTypes;
+using AutoMapper;
 
 namespace LeaveManagementSystem.Web.Controllers
 {
     public class LeaveTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public LeaveTypesController(ApplicationDbContext context)
+        public LeaveTypesController(ApplicationDbContext context,IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: LeaveTypes
@@ -26,12 +29,16 @@ namespace LeaveManagementSystem.Web.Controllers
             var data = await _context.LeaveTypes.ToListAsync();
 
             //convert the data model into view model
-            var viewData = data.Select(q => new IndexVM
-            {
-               Id = q.Id,
-               Name = q.Name,
-               Days = q.NumberOfDays
-            });
+            //var viewData = data.Select(q => new IndexVM
+            //{
+            //   Id = q.Id,
+            //   Name = q.Name,
+            //   Days = q.NumberOfDays
+            //});
+
+            //convert the data model into view model using AutoMapper
+            var viewData = _mapper.Map<List<IndexVM>>(data);
+
             //return the view model to the view.
             //return View(data);
             return View(viewData);
