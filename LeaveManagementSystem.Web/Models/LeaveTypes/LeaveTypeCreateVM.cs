fce8fdacaccc1188388c97fa984b9b0a789b46ea
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-02-25 00:13:08 +0530

    Inherited the base class in ReadOnly & Edit VM classes and added annotations for changing labels

--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeCreateVM.cs
@@ -7,8 +7,10 @@ namespace LeaveManagementSystem.Web.Models.LeaveTypes
         [Required]
         [Length(4,25,ErrorMessage ="You have violated the characters limit")]
         public string Name { get; set; } = string.Empty;
+
         [Required]
         [Range(1,90,ErrorMessage ="You have violated the days limit")]
+        [Display(Name = "Maximum Allocation of Days")]
         public int Days { get; set; }
     }
 }
--- LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
@@ -2,14 +2,15 @@
 
 namespace LeaveManagementSystem.Web.Models.LeaveTypes
 {
-    public class LeaveTypeEditVM
+    public class LeaveTypeEditVM : BaseLeaveTypeVM
     {
-        public int Id { get; set; }
         [Required]
         [Length(4, 25, ErrorMessage = "You have violated the characters limit")]
         public string Name { get; set; } = string.Empty;
+
         [Required]
         [Range(1, 90, ErrorMessage = "You have violated the days limit")]
+        [Display(Name = "Maximum Allocation of Days")]
         public int Days { get; set; }
     }
 }
