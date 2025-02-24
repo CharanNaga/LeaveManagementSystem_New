c29c5b5d5020623c3bf92ca3d4f5efafba38fe81
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-02-24 23:53:09 +0530

    Created a new VM for Editing

---
+++ LeaveManagementSystem.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Models.LeaveTypes
+{
+    public class LeaveTypeEditVM
+    {
+        public int Id { get; set; }
+        [Required]
+        [Length(4, 25, ErrorMessage = "You have violated the characters limit")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Range(1, 90, ErrorMessage = "You have violated the days limit")]
+        public int Days { get; set; }
+    }
+}
