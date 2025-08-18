245c04d93a269cf32e71b139e6ad1dfcd805b551
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-08-18 23:27:23 +0530

    Created a new static class for holding role names and added it to Global usings

--- LeaveManagementSystem.Web/GlobalUsings.cs
+++ LeaveManagementSystem.Web/GlobalUsings.cs
@@ -9,3 +9,4 @@ global using System.ComponentModel.DataAnnotations;
 global using Microsoft.AspNetCore.WebUtilities;
 global using System.Text;
 global using System.Text.Encodings.Web;
+global using LeaveManagementSystem.Web.Common;
