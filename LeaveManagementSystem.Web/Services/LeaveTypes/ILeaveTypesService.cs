26726a9fc2cf9f1779dbaa90efc8c74f2f1e9321
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-08-19 20:25:41 +0530

    Moved Email & Leave Types Services into seperate folders

---
+++ LeaveManagementSystem.Web/Services/LeaveTypes/ILeaveTypesService.cs
@@ -0,0 +1,16 @@
+﻿using LeaveManagementSystem.Web.Models.LeaveTypes;
+
+namespace LeaveManagementSystem.Web.Services.LeaveTypes
+{
+    public interface ILeaveTypesService
+    {
+        Task<bool> CheckIfLeaveTypeNameAlreadyExists(string name);
+        Task<bool> CheckIfLeaveTypeNameAlreadyExistsForEdit(LeaveTypeEditVM leaveTypeEdit);
+        Task Create(LeaveTypeCreateVM model);
+        Task Edit(LeaveTypeEditVM model);
+        Task<T?> Get<T>(int id) where T : class;
+        Task<List<LeaveTypeReadOnlyVM>> GetAll();
+        bool LeaveTypeExists(int id);
+        Task Remove(int id);
+    }
+}
---
+++ LeaveManagementSystem.Web/Services/LeaveTypes/LeaveTypesService.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Web.Services.LeaveTypes
+{
+    public class LeaveTypesService(ApplicationDbContext _context, IMapper _mapper) : ILeaveTypesService
+    {
+        public async Task<List<LeaveTypeReadOnlyVM>> GetAll()
+        {
+            //var data = 'SELECT * FROM LeaveTypes';
+            var data = await _context.LeaveTypes.ToListAsync();
+
+            //convert the data model into view model using AutoMapper
+            var viewData = _mapper.Map<List<LeaveTypeReadOnlyVM>>(data);
+
+            return viewData;
+        }
+
+        public async Task<T?> Get<T>(int id) where T : class
+        {
+            //var leaveType = 'SELECT * FROM LeaveTypes where Id = @id';
+
+            var leaveType = await _context.LeaveTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (leaveType == null)
+            {
+                return null;
+            }
+
+            //convert data model into view model using AutoMapper
+            var viewData = _mapper.Map<T>(leaveType);
+            return viewData;
+        }
+
+        public async Task Remove(int id)
+        {
+            var leaveType = await _context.LeaveTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (leaveType != null)
+            {
+                _context.Remove(leaveType);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Edit(LeaveTypeEditVM model)
+        {
+            var leaveType = _mapper.Map<LeaveType>(model);
+            _context.Update(leaveType);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Create(LeaveTypeCreateVM model)
+        {
+            var leaveType = _mapper.Map<LeaveType>(model);
+            _context.Add(leaveType);
+            await _context.SaveChangesAsync();
+        }
+
+        public bool LeaveTypeExists(int id)
+        {
+            return _context.LeaveTypes.Any(e => e.Id == id);
+        }
+
+        public async Task<bool> CheckIfLeaveTypeNameAlreadyExists(string name)
+        {
+            var lowerCaseName = name.ToLower();
+            return await _context.LeaveTypes.AnyAsync(l => l.Name.ToLower().Equals(lowerCaseName));
+        }
+
+        public async Task<bool> CheckIfLeaveTypeNameAlreadyExistsForEdit(LeaveTypeEditVM leaveTypeEdit)
+        {
+            var lowerCaseName = leaveTypeEdit.Name.ToLower();
+            return await _context.LeaveTypes.AnyAsync(
+                l => l.Name.ToLower().Equals(lowerCaseName)
+                && l.Id != leaveTypeEdit.Id);
+        }
+    }
+}
