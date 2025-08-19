069fa9d0bb4afe3e52eec0ebd9bc11441a89b35f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-08-19 20:27:48 +0530

    Set up the Leave Allocation Service

--- LeaveManagementSystem.Web/Program.cs
+++ LeaveManagementSystem.Web/Program.cs
@@ -1,5 +1,7 @@
 using LeaveManagementSystem.Web.Data;
-using LeaveManagementSystem.Web.Services;
+using LeaveManagementSystem.Web.Services.Email;
+using LeaveManagementSystem.Web.Services.LeaveAllocations;
+using LeaveManagementSystem.Web.Services.LeaveTypes;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddScoped<ILeaveTypesService, LeaveTypesService>();
+builder.Services.AddScoped<ILeaveAllocationsService, LeaveAllocationsService>();
 
 builder.Services.AddTransient<IEmailSender,EmailSender>();
 
