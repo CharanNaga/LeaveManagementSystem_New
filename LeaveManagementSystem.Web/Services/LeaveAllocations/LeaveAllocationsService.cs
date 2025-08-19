797e94d63525281d7055cdce4d1bdd9a72b87aaf
CharanNaga <nagasaicharansinghbondili@gmail.com>
2025-08-19 22:44:59 +0530

    Set up Employee Leave Allocation Logic in Service class

--- LeaveManagementSystem.Web/Services/LeaveAllocations/LeaveAllocationsService.cs
+++ LeaveManagementSystem.Web/Services/LeaveAllocations/LeaveAllocationsService.cs
@@ -1,7 +1,36 @@
-﻿namespace LeaveManagementSystem.Web.Services.LeaveAllocations
-{
-    public class LeaveAllocationsService : ILeaveAllocationsService
-    {
+﻿
+using Microsoft.EntityFrameworkCore;
 
+namespace LeaveManagementSystem.Web.Services.LeaveAllocations
+{
+    public class LeaveAllocationsService(ApplicationDbContext _context) : ILeaveAllocationsService
+    {
+        public async Task AllocateLeave(string employeeId)
+        {
+            // Get all the Leave Types
+            var leaveTypes =  await _context.LeaveTypes.ToListAsync();
+
+           // Get the current period based on the year
+           var currentDate = DateTime.Now;
+           var period = await _context.Periods.SingleAsync(p=>p.EndDate.Year == currentDate.Year);
+
+           // Calculate leave based on number of months left in the period
+           var monthsRemaining = period.EndDate.Month - currentDate.Month;
+
+            // Foreach leave type, create an allocation entry
+            foreach (var leaveType in leaveTypes)
+            {
+                var accuralRate = decimal.Divide(leaveType.NumberOfDays, 12);
+                var leaveAllocation = new LeaveAllocation
+                {
+                    EmployeeId = employeeId,
+                    LeaveTypeId = leaveType.Id,
+                    PeriodId = period.Id,
+                    Days = (int)Math.Ceiling(accuralRate * monthsRemaining)
+                };
+                _context.Add(leaveAllocation);
+            }
+            await _context.SaveChangesAsync();
+        }
     }
 }
